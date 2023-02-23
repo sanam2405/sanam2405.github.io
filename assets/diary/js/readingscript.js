7668fe904758b0ca68d635a429824995cf9fa815
Manas Pratim Biswas <75798265+sanam2405@users.noreply.github.com>
2023-02-23 16:55:27 +0530

    Update readingscript.js

--- assets/diary/js/readingscript.js
+++ assets/diary/js/readingscript.js
@@ -1,92 +1,129 @@
-
 function addReadingColor() {
-  document.querySelector("body").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("body").style.backgroundColor = 'rgb(255, 255, 191)';
-  document.querySelector("#page-wrapper").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#page-wrapper").style.backgroundColor = 'rgb(255, 255, 191)';
-  document.querySelector("#header").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#header").style.backgroundColor = 'rgb(255, 255, 191)';
-  document.querySelector("#main").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#main").style.backgroundColor = 'rgb(255, 255, 191)';
-  document.querySelector("form").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("form").style.backgroundColor = 'rgb(255, 255, 191)';
-  document.querySelector("textarea").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("textarea").style.backgroundColor = 'rgb(255, 255, 191)';
-  
-  const containerElements = document.querySelectorAll('.container');
-  containerElements.forEach(function(element) {
+  document.querySelector("body").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("body").style.backgroundColor = "rgb(255, 255, 191)";
+  document.querySelector("#page-wrapper").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#page-wrapper").style.backgroundColor =
+    "rgb(255, 255, 191)";
+  document.querySelector("#header").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#header").style.backgroundColor =
+    "rgb(255, 255, 191)";
+  document.querySelector("#main").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#main").style.backgroundColor = "rgb(255, 255, 191)";
+  document.querySelector("form").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("form").style.backgroundColor = "rgb(255, 255, 191)";
+  document.querySelector("textarea").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("textarea").style.backgroundColor =
+    "rgb(255, 255, 191)";
+
+  const containerElements = document.querySelectorAll(".container");
+  containerElements.forEach(function (element) {
     element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = 'rgb(255, 255, 191)';
+    element.style.background = "rgb(255, 255, 191)";
   });
-  
-  const inputElements = document.querySelectorAll('input');
-  inputElements.forEach(function(element) {
+
+  const inputElements = document.querySelectorAll("input");
+  inputElements.forEach(function (element) {
     element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = 'rgb(255, 255, 191)';
+    element.style.background = "rgb(255, 255, 191)";
   });
-  
-  const mainElements = document.querySelectorAll('#main');
-  mainElements.forEach(function(element) {
+
+  const mainElements = document.querySelectorAll("#main");
+  mainElements.forEach(function (element) {
     element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = 'rgb(255, 255, 191)';
+    element.style.background = "rgb(255, 255, 191)";
   });
-  
-  document.querySelector("#footer").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#footer").style.backgroundColor = 'rgb(255, 255, 191)';
-  document.getElementById("canvas").style.transition = "background-color 3s ease-in-out";
-  document.getElementById("canvas").style.backgroundColor = 'rgb(255, 255, 191)';
+
+  document.querySelector("#footer").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#footer").style.backgroundColor =
+    "rgb(255, 255, 191)";
+  document.getElementById("canvas").style.transition =
+    "background-color 3s ease-in-out";
+  document.getElementById("canvas").style.backgroundColor =
+    "rgb(255, 255, 191)";
 }
-
-
 
 function removeReadingColor() {
-  document.querySelector("body").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("body").style.backgroundColor = 'rgb(255, 255, 255)';
-  document.querySelector("#page-wrapper").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#page-wrapper").style.backgroundColor = 'rgb(255, 255, 255)';
-  document.querySelector("#header").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#header").style.backgroundColor = 'rgb(255, 255, 255)';
-  document.querySelector("#main").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#main").style.backgroundColor = 'rgb(255, 255, 255)';
-  document.querySelector("form").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("form").style.backgroundColor = 'rgb(255, 255, 255)';
-  document.querySelector("textarea").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("textarea").style.backgroundColor = 'rgb(255, 255, 255)';
-  
-  const containerElements = document.querySelectorAll('.container');
-  containerElements.forEach(function(element) {
+  document.querySelector("body").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("body").style.backgroundColor = "rgb(255, 255, 255)";
+  document.querySelector("#page-wrapper").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#page-wrapper").style.backgroundColor =
+    "rgb(255, 255, 255)";
+  document.querySelector("#header").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#header").style.backgroundColor =
+    "rgb(255, 255, 255)";
+  document.querySelector("#main").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#main").style.backgroundColor = "rgb(255, 255, 255)";
+  document.querySelector("form").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("form").style.backgroundColor = "rgb(255, 255, 255)";
+  document.querySelector("textarea").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("textarea").style.backgroundColor =
+    "rgb(255, 255, 255)";
+
+  const containerElements = document.querySelectorAll(".container");
+  containerElements.forEach(function (element) {
     element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = 'rgb(255, 255, 255)';
+    element.style.background = "rgb(255, 255, 255)";
   });
-  
-  const inputElements = document.querySelectorAll('input');
-  inputElements.forEach(function(element) {
+
+  const inputElements = document.querySelectorAll("input");
+  inputElements.forEach(function (element) {
     element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = 'rgb(255, 255, 255)';
+    element.style.background = "rgb(255, 255, 255)";
   });
-  
-  const mainElements = document.querySelectorAll('#main');
-  mainElements.forEach(function(element) {
+
+  const mainElements = document.querySelectorAll("#main");
+  mainElements.forEach(function (element) {
     element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = 'rgb(255, 255, 255)';
+    element.style.background = "rgb(255, 255, 255)";
   });
-  
-  document.querySelector("#footer").style.transition = "background-color 3s ease-in-out";
-  document.querySelector("#footer").style.backgroundColor = 'rgb(255, 255, 255)';
-  document.getElementById("canvas").style.transition = "background-color 3s ease-in-out";
-  document.getElementById("canvas").style.backgroundColor = 'rgb(255, 255, 255)';
+
+  document.querySelector("#footer").style.transition =
+    "background-color 3s ease-in-out";
+  document.querySelector("#footer").style.backgroundColor =
+    "rgb(255, 255, 255)";
+  document.getElementById("canvas").style.transition =
+    "background-color 3s ease-in-out";
+  document.getElementById("canvas").style.backgroundColor =
+    "rgb(255, 255, 255)";
 }
 
+const wrapper = document.querySelector(".wrapper");
+const hideCheckbox = document.querySelector("#hide-checkbox");
 
-const wrapper = document.querySelector('.wrapper');
+document.addEventListener("DOMContentLoaded", function () {
 
-wrapper.addEventListener('change', function(e) {
-  if(e.target.checked) {
+  // Check local storage for previous state of checkbox
+  if (localStorage.getItem("hideCheckboxState") === "checked") {
+    hideCheckbox.checked = true;
     addReadingColor();
   } else {
+    hideCheckbox.checked = false;
     removeReadingColor();
   }
-})
+});
 
-
-
+// Handle checkbox change event
+wrapper.addEventListener("change", function (e) {
+  if (e.target === hideCheckbox) {
+    if (e.target.checked) {
+      addReadingColor();
+      localStorage.setItem("hideCheckboxState", "checked"); // Save state to local storage
+    } else {
+      removeReadingColor();
+      localStorage.removeItem("hideCheckboxState"); // Remove state from local storage
+    }
+  }
+});
