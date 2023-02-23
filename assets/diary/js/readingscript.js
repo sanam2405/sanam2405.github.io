97e8a9da8c05135fd6139f570acc15d55b3392de
Manas Pratim Biswas <75798265+sanam2405@users.noreply.github.com>
2023-02-23 18:05:16 +0530

    Update readingscript.js

--- assets/diary/js/readingscript.js
+++ assets/diary/js/readingscript.js
@@ -102,11 +102,9 @@ function removeReadingColor() {
 
 const wrapper = document.querySelector(".wrapper");
 const hideCheckbox = document.querySelector("#hide-checkbox");
+var currentState = localStorage.getItem("hideCheckboxState");
 
 document.addEventListener("DOMContentLoaded", function () {
-
-
-  var currentState = localStorage.getItem("hideCheckboxState")
 
   if(currentState === null) {
     currentState = false;
@@ -142,159 +140,11 @@ wrapper.addEventListener("change", function (e) {
       localStorage.setItem("hideCheckboxState", currentState); // Remove state from local storage
     }
 
-    if (currentState === "checked")
+    if (currentState === true)
     console.log("checked\n");
     else
     console.log("unchecked\n");
   }
-});
-function addReadingColor() {
-  document.querySelector("body").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("body").style.backgroundColor = "rgb(255, 255, 191)";
-  document.querySelector("#page-wrapper").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#page-wrapper").style.backgroundColor =
-    "rgb(255, 255, 191)";
-  document.querySelector("#header").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#header").style.backgroundColor =
-    "rgb(255, 255, 191)";
-  document.querySelector("#main").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#main").style.backgroundColor = "rgb(255, 255, 191)";
-  document.querySelector("form").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("form").style.backgroundColor = "rgb(255, 255, 191)";
-  document.querySelector("textarea").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("textarea").style.backgroundColor =
-    "rgb(255, 255, 191)";
 
-  const containerElements = document.querySelectorAll(".container");
-  containerElements.forEach(function (element) {
-    element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = "rgb(255, 255, 191)";
-  });
-
-  const inputElements = document.querySelectorAll("input");
-  inputElements.forEach(function (element) {
-    element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = "rgb(255, 255, 191)";
-  });
-
-  const mainElements = document.querySelectorAll("#main");
-  mainElements.forEach(function (element) {
-    element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = "rgb(255, 255, 191)";
-  });
-
-  document.querySelector("#footer").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#footer").style.backgroundColor =
-    "rgb(255, 255, 191)";
-  document.getElementById("canvas").style.transition =
-    "background-color 3s ease-in-out";
-  document.getElementById("canvas").style.backgroundColor =
-    "rgb(255, 255, 191)";
-}
-
-function removeReadingColor() {
-  document.querySelector("body").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("body").style.backgroundColor = "rgb(255, 255, 255)";
-  document.querySelector("#page-wrapper").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#page-wrapper").style.backgroundColor =
-    "rgb(255, 255, 255)";
-  document.querySelector("#header").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#header").style.backgroundColor =
-    "rgb(255, 255, 255)";
-  document.querySelector("#main").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#main").style.backgroundColor = "rgb(255, 255, 255)";
-  document.querySelector("form").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("form").style.backgroundColor = "rgb(255, 255, 255)";
-  document.querySelector("textarea").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("textarea").style.backgroundColor =
-    "rgb(255, 255, 255)";
-
-  const containerElements = document.querySelectorAll(".container");
-  containerElements.forEach(function (element) {
-    element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = "rgb(255, 255, 255)";
-  });
-
-  const inputElements = document.querySelectorAll("input");
-  inputElements.forEach(function (element) {
-    element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = "rgb(255, 255, 255)";
-  });
-
-  const mainElements = document.querySelectorAll("#main");
-  mainElements.forEach(function (element) {
-    element.style.transition = "background-color 3s ease-in-out";
-    element.style.background = "rgb(255, 255, 255)";
-  });
-
-  document.querySelector("#footer").style.transition =
-    "background-color 3s ease-in-out";
-  document.querySelector("#footer").style.backgroundColor =
-    "rgb(255, 255, 255)";
-  document.getElementById("canvas").style.transition =
-    "background-color 3s ease-in-out";
-  document.getElementById("canvas").style.backgroundColor =
-    "rgb(255, 255, 255)";
-}
-
-const wrapper = document.querySelector(".wrapper");
-const hideCheckbox = document.querySelector("#hide-checkbox");
-
-document.addEventListener("DOMContentLoaded", function () {
-
-
-  var currentState = localStorage.getItem("hideCheckboxState")
-
-  if(currentState === null) {
-    currentState = false;
-    localStorage.setItem("hideCheckboxState",false);
-  }
-
-  // Check local storage for previous state of checkbox
-  if (currentState === true) {
-    hideCheckbox.checked = true;
-    addReadingColor();
-  } else {
-    hideCheckbox.checked = false;
-    removeReadingColor();
-  }
-
-  if (currentState === true)
-  console.log("checked\n");
-  else
-  console.log("unchecked\n");
-
-});
-
-// Handle checkbox change event
-wrapper.addEventListener("change", function (e) {
-  if (e.target === hideCheckbox) {
-    if (e.target.checked) {
-      addReadingColor();
-      currentState = true;
-      localStorage.setItem("hideCheckboxState", currentState); // Save state to local storage
-    } else {
-      removeReadingColor();
-      currentState = false;
-      localStorage.setItem("hideCheckboxState", currentState); // Remove state from local storage
-    }
-
-    if (currentState === "checked")
-    console.log("checked\n");
-    else
-    console.log("unchecked\n");
-  }
+  currentState = false;
 });
